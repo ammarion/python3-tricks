0209da1c29ab8e3c4e2e320757e91343fd70eb08
Ammar Alim <ammar.alim@nordstrom.com>
2019-05-14 20:04:10 -0700

    new changes to the file

--- bus/bus.py
+++ bus/bus.py
@@ -1,14 +1,17 @@
 # coding: utf-8
-get_ipython().run_line_magic('clear', '')
+import sys
+from xml.etree.ElementTree import XML
 import urllib.request
-u = urllib.request.urlopen('http://ctabustracker.com/bustime/map/getStopPredictions.jsp?stop=1487&route=22')
+if len(sys.argv) != 3:
+    raise SystemExit('Usage: bus.py route stopid')
+route = sys.argv[1]
+stopid = sys.argv[2]
+
+u = urllib.request.urlopen('http://ctabustracker.com/bustime/map/getStopPredictions.jsp?stop={}&route={}'.format(route, stopid))
 u
 data = u.read()
-data
-from xml.etree.ElementTree import XML
 doc = XML(data)
-doc
-for pt in doc.findall('.//pt')
+
 for pt in doc.findall('.//pt'):
     print(pt.text)
     
