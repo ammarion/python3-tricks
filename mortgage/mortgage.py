c0729d6afcc92ec2d1722e5715cdd011ad9403fc
Ammar Alim <ammar.alim@nordstrom.com>
2019-05-15 20:17:01 -0700

    part 3

--- mortgage/mortgage.py
+++ mortgage/mortgage.py
@@ -9,6 +9,9 @@ extra_payment_start_month = 1
 extra_payment_end_month = 60
 month = 0
 
+out = open('schedule.txt', 'w') # Open a file for writing
+
+print('{:>5s} {:>10s} {:>10s} {:>10s}'.format('Month', 'Interest', 'Principal', 'Remaining'), file=out)
 while principal > 0:
     month += 1
     if month >= extra_payment_start_month and month <= extra_payment_end_month:
@@ -18,5 +21,6 @@ while principal > 0:
     interest = principal*(rate/12)
     principal = principal + interest - total_payment
     total_paid += total_payment
-
-print('Total Paid: ', total_paid)
+    print('{:>5d} {:>10.2f} {:>10.2f}{:>10.2f}'.format(month, interest, total_payment - interest, principal), file=out)
+out.close()
+print("Total Paid: ", str(total_paid))
