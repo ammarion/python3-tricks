49eb7d855d51a168d58c2e77e78163c5407be278
Ammar Alim <ammar.alim@nordstrom.com>
2019-05-17 08:39:50 -0700

    functions

--- mortgage/port.py
+++ mortgage/port.py
@@ -1,12 +1,22 @@
 # port.py
 import csv
-total = 0.0
-with open('Book.csv', 'r') as f:
-    rows = csv.reader(f)
-    headers = next(rows)   # Skip a single input
-    for row in rows:
-        row[2] = int(row[2])
-        row[3] = float(row[3])
-        total = row[2]*row[3]
 
-print(total)
+def portfolio_cost(filename):
+    total = 0.0
+    with open('Book.csv', 'r') as f:
+        rows = csv.reader(f)
+        headers = next(rows)   # Skip a single input
+        for rowno, row in enumerate(rows, start=1):
+            try:
+                row[2] = int(row[2])
+                row[3] = float(row[3])
+            except ValueError as err:
+                print('Row:', rowno, 'Bad row:', row)
+                print('Row:', rowno, 'Reason:', err)
+                continue    # Skips to the next row
+            total += row[2]*row[3]
+
+    return total
+
+total = portfolio_cost('Book.csv')
+print('Total Cost: ', total)
