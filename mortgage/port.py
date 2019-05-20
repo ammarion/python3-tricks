e063e5336128f88f1c15e3c41af4d9c7f5d10ed2
Ammar Alim <ammar.alim@nordstrom.com>
2019-05-20 09:42:51 -0700

    ec2 management

--- mortgage/port.py
+++ mortgage/port.py
@@ -1,8 +1,15 @@
 # port.py
 import csv
+# Don't catch all errors by using except exception
+# print statements 
 
-def portfolio_cost(filename):
-    total = 0.0
+def read_portfolio(filename, *, errors='warn'):
+    """
+    Read a csv file when name, date, shares, price data into a list
+    """
+    if errors not in {'warn', 'silent', 'raise'}:
+        raise ValueError("Error must be one of 'warn', 'silent', 'raise'")
+    portfolio= []
     with open('Book.csv', 'r') as f:
         rows = csv.reader(f)
         headers = next(rows)   # Skip a single input
@@ -11,12 +18,31 @@ def portfolio_cost(filename):
                 row[2] = int(row[2])
                 row[3] = float(row[3])
             except ValueError as err:
-                print('Row:', rowno, 'Bad row:', row)
-                print('Row:', rowno, 'Reason:', err)
-                continue    # Skips to the next row
-            total += row[2]*row[3]
+                if errors == warn:
+                    print('Row:', rowno, 'Bad row:', row)
+                    print('Row:', rowno, 'Reason:', err)
+                elif errors == 'raise':  # Reraises the last exception
+                    raise
+                else:
+                    pass # Ignore
+                continue  # Skipts to the next line
+            # record = tuple(row)
+            record = {
+                'name': row[0],
+                'date': row[1],
+                'shares': row[2],
+                'price': row[3]
+            }
+            portfolio.append(record)
+    return  portfolio
 
-    return total
+portfolio = read_portfolio('Book.csv')
+print(portfolio)
 
-total = portfolio_cost('Book.csv')
+total = 0.0
+
+for holding in portfolio:
+    total += holding['shares'] * holding['price']   #shares * price
 print('Total Cost: ', total)
+# total = portfolio_cost('Book.csv', errors='warn')
+# print('Total Cost: ', total)
