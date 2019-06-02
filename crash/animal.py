a468bafe1917bcf1d1f985f251af55eb4e3c6f7d
Ammar Alim <ammar.alim@nordstrom.com>
2019-06-02 11:13:32 -0700

    Raynor iam policies

---
+++ crash/animal.py
@@ -0,0 +1,14 @@
+def describe_pet(pet_name, animal_type='dog'):
+    """Display animal type"""
+    print(f'\nI have a {animal_type}.')
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+
+
+
+# describe_pet('Human', 'ali')
+# describe_pet('dog', 'willie')
+# describe_pet(pet_name='ahmed')
+describe_pet('david')
+describe_pet(pet_name='harry', animal_type='hamster')
