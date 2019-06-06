f63471044bfbdd091bf8670a9208b4d6f5bb4b25
Ammar Alim <ammar.alim@nordstrom.com>
2019-06-06 11:56:08 -0700

    chapter 8 exercies.....

---
+++ crash/8-15.py
@@ -0,0 +1,7 @@
+import printed
+from printed import show_completed_models
+from printed import show_completed_models as complted
+import printed as mn
+from printed import *
+
+printed.show_completed_models('xxxx')
