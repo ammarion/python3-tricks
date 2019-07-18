55dc7adf3a7be58bd118c18756e037994e9c037a
Ammar Alim <ammar.alim@nordstrom.com>
2019-07-18 08:56:50 -0700

    docker

---
+++ bash/myscript.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /tmp 2>/dev/null || { echo cd failed ; exit 1 ; }
+
+echo continuing onclear
---
+++ bash/phone.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+grep -i $* <<EOF
+bob 555-1212
+bill 555-1234
+john 555-2323
+EOF
